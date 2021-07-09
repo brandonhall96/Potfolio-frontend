fba59cbb576d52ee0d220a88fc93389226d49b98
brandonhall96 <hall.bran96@gmail.com>
2021-07-09 14:28:05 -0400

    added wallpaper working on social buttons

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,6 +4,12 @@ const Welcome = () => {
     return (
         <div>
             <h1>Welcome to My Portfolio!</h1>
+            <div>
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-linkedin"></a>
+            <a href="#" class="fa fa-github"></a>
+            </div>
         </div>
     )
 }
