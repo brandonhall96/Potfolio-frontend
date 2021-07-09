946346eaecdd6a4ffe6efed37094b14bf904ec23
brandonhall96 <hall.bran96@gmail.com>
2021-07-09 14:53:34 -0400

    added headshot and social buttons

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+
 const Welcome = () => {
     return (
         <div>
+            <img id="headshot" src="images/portpropic.jpeg" alt='profile picture'></img>
             <h1>Welcome to My Portfolio!</h1>
             <div>
             <a href="#" class="fa fa-facebook"></a>
